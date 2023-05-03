67a26ced40b9bd448308b41f9b873e813cc7b5cf
Dang Tran <hdang.sea@gmail.com>
2023-05-02 23:05:47 -0500

    revert back to original home.py

--- Home.py
+++ Home.py
@@ -37,84 +37,83 @@ def load_us_employment_data():
         + "FROM bls_employment_timeseries as ts "
         + "JOIN cybersyn.bls_employment_attributes AS att ON (ts.variable = att.variable) "
         + "WHERE geo_id = 'country/USA' AND att.unit = 'Level' AND REPORT = 'JOLTS' AND FREQUENCY = 'Annual'")
-    return cur.fetchall()
+    return cur.fetch_pandas_all()
 
 us_anual_cpi_df = load_us_annual_cpi_data()
-st.dataframe(us_anual_cpi_df)
-# us_anual_cpi_df['YEAR'] = pd.DatetimeIndex(us_anual_cpi_df['DATE']).year
-# us_anual_cpi_df= us_anual_cpi_df.sort_values(by=['YEAR'])
+us_anual_cpi_df['YEAR'] = pd.DatetimeIndex(us_anual_cpi_df['DATE']).year
+us_anual_cpi_df= us_anual_cpi_df.sort_values(by=['YEAR'])
 
-# def build_category_cpi_df(key, variable):
-#     df = us_anual_cpi_df[us_anual_cpi_df['VARIABLE'] == variable];
-#     df[key] = df['VALUE']
-#     df = df.filter(items=[key, 'YEAR'])
-#     df.set_index('YEAR', inplace=True)
+def build_category_cpi_df(key, variable):
+    df = us_anual_cpi_df[us_anual_cpi_df['VARIABLE'] == variable];
+    df[key] = df['VALUE']
+    df = df.filter(items=[key, 'YEAR'])
+    df.set_index('YEAR', inplace=True)
 
-#     return df
+    return df
 
-# food_df = build_category_cpi_df('FOOD', 'CPI:_Food,_Not_seasonally_adjusted,_Annual')
-# all_items_df = build_category_cpi_df('ALL ITEMS', 'CPI:_All_items,_Not_seasonally_adjusted,_Annual')
-# energy_df = build_category_cpi_df('ENERGY', 'CPI:_Energy,_Not_seasonally_adjusted,_Annual')
-# all_items_less_food_and_energy_df = build_category_cpi_df('ALL ITEMS LESS FOOD AND ENERGY', 'CPI:_All_items_less_food_and_energy,_Not_seasonally_adjusted,_Annual')
+food_df = build_category_cpi_df('FOOD', 'CPI:_Food,_Not_seasonally_adjusted,_Annual')
+all_items_df = build_category_cpi_df('ALL ITEMS', 'CPI:_All_items,_Not_seasonally_adjusted,_Annual')
+energy_df = build_category_cpi_df('ENERGY', 'CPI:_Energy,_Not_seasonally_adjusted,_Annual')
+all_items_less_food_and_energy_df = build_category_cpi_df('ALL ITEMS LESS FOOD AND ENERGY', 'CPI:_All_items_less_food_and_energy,_Not_seasonally_adjusted,_Annual')
 
-# main_categories_cpi_df = pd.merge(left=all_items_df, right=food_df, how='outer', left_index=True, right_index=True)
-# main_categories_cpi_df = main_categories_cpi_df.merge(energy_df, how='outer', left_index=True, right_index=True)
-# main_categories_cpi_df = main_categories_cpi_df.merge(all_items_less_food_and_energy_df, how='outer', left_on='YEAR', right_on='YEAR')
+main_categories_cpi_df = pd.merge(left=all_items_df, right=food_df, how='outer', left_index=True, right_index=True)
+main_categories_cpi_df = main_categories_cpi_df.merge(energy_df, how='outer', left_index=True, right_index=True)
+main_categories_cpi_df = main_categories_cpi_df.merge(all_items_less_food_and_energy_df, how='outer', left_on='YEAR', right_on='YEAR')
 
-# with st.container():
-#     st.header('Consumer Price Index (CPI)')
-#     st.text('CPI is a measure of the average change over time in the prices paid by urban consumers for a market basket of consumer goods and services.')
-#     col1, col2 = st.columns([3, 1])
+with st.container():
+    st.header('Consumer Price Index (CPI)')
+    st.text('CPI is a measure of the average change over time in the prices paid by urban consumers for a market basket of consumer goods and services.')
+    col1, col2 = st.columns([3, 1])
 
-#     end_year = int(main_categories_cpi_df.index.max())
-#     start_year = end_year - 20
-#     col1.subheader('Years')
-#     selected_range_start, selected_range_end = col1.select_slider('Select a range of years to see how CPI has changed.', options=main_categories_cpi_df.index, value=(start_year, end_year))
-#     main_categories_cpi_df = main_categories_cpi_df[main_categories_cpi_df.index >= selected_range_start]
-#     main_categories_cpi_df = main_categories_cpi_df[main_categories_cpi_df.index <= selected_range_end]
-#     main_categories_cpi_df.index = main_categories_cpi_df.index.astype(str)
-#     col1.line_chart(main_categories_cpi_df)
-
-
-#     if (col2.checkbox('Show data', key='show_cpi_data')):
-#         col2.dataframe(main_categories_cpi_df)
-
-# us_employment_df = load_us_employment_data()
-# us_employment_df['YEAR'] = pd.DatetimeIndex(us_employment_df['DATE']).year
-
-# def build_employment_dataframe(key, measure):
-#     df = us_employment_df[us_employment_df['MEASURE'] == measure]
-#     df = df.groupby('YEAR', as_index = False)['VALUE'].sum()
-#     df[key] = df['VALUE']
-#     df = df.filter(items=['YEAR', key])
-#     df.set_index('YEAR', inplace=True)
-
-#     return df
+    end_year = int(main_categories_cpi_df.index.max())
+    start_year = end_year - 20
+    col1.subheader('Years')
+    selected_range_start, selected_range_end = col1.select_slider('Select a range of years to see how CPI has changed.', options=main_categories_cpi_df.index, value=(start_year, end_year))
+    main_categories_cpi_df = main_categories_cpi_df[main_categories_cpi_df.index >= selected_range_start]
+    main_categories_cpi_df = main_categories_cpi_df[main_categories_cpi_df.index <= selected_range_end]
+    main_categories_cpi_df.index = main_categories_cpi_df.index.astype(str)
+    col1.line_chart(main_categories_cpi_df)
 
 
-# layoffs_df = build_employment_dataframe('LAYOFFS', 'Layoffs and discharges')
-# hires_df = build_employment_dataframe('HIRES', 'Hires')
-# job_openings_df = build_employment_dataframe('JOB OPENINGS', 'Job openings')
-# other_separations_df = build_employment_dataframe('OTHER SEPARATIONS', 'Other separations')
-# quits_df = build_employment_dataframe('QUITS', 'Quits')
+    if (col2.checkbox('Show data', key='show_cpi_data')):
+        col2.dataframe(main_categories_cpi_df)
 
-# employment_df = pd.merge(left=job_openings_df, right=hires_df, how='outer', left_index=True, right_index=True)
-# employment_df = employment_df.merge(quits_df, how='outer', left_index=True, right_index=True)
-# employment_df = employment_df.merge(layoffs_df, how='outer', left_index=True, right_index=True)
-# employment_df = employment_df.merge(other_separations_df, how='outer', left_index=True, right_index=True)
+us_employment_df = load_us_employment_data()
+us_employment_df['YEAR'] = pd.DatetimeIndex(us_employment_df['DATE']).year
 
-# with st.container():
-#     st.header('Job Openings and Labor Turnover Survey (JOLTS)')
-#     st.text('JOLTS provides data on job openings, hires, and separations at the national and state level. The job openings rate can help measure the tightness of job markets.')
-#     st.markdown('* Job Openings: All positions that are open (not filled) on the last business day of the month.')
-#     st.markdown('* Hires: All additions to the payroll during the month.')
-#     st.markdown('* Quits: Employees who left voluntarily. Exception: retirements or transfers to other locations are reported with Other Separations.')
-#     st.markdown('* Layoffs & Discharges: Involuntary separations initiated by the employer.')
-#     st.markdown('* Other Separations: Retirements, transfers to other locations; deaths; or separations due to employee disability.')
+def build_employment_dataframe(key, measure):
+    df = us_employment_df[us_employment_df['MEASURE'] == measure]
+    df = df.groupby('YEAR', as_index = False)['VALUE'].sum()
+    df[key] = df['VALUE']
+    df = df.filter(items=['YEAR', key])
+    df.set_index('YEAR', inplace=True)
 
-#     col1, col2 = st.columns([3, 1])
+    return df
 
-#     col1.line_chart(employment_df)
-#     if (col2.checkbox('Show data', key='show_jolts_data')):
-#         col2.dataframe(employment_df)
+
+layoffs_df = build_employment_dataframe('LAYOFFS', 'Layoffs and discharges')
+hires_df = build_employment_dataframe('HIRES', 'Hires')
+job_openings_df = build_employment_dataframe('JOB OPENINGS', 'Job openings')
+other_separations_df = build_employment_dataframe('OTHER SEPARATIONS', 'Other separations')
+quits_df = build_employment_dataframe('QUITS', 'Quits')
+
+employment_df = pd.merge(left=job_openings_df, right=hires_df, how='outer', left_index=True, right_index=True)
+employment_df = employment_df.merge(quits_df, how='outer', left_index=True, right_index=True)
+employment_df = employment_df.merge(layoffs_df, how='outer', left_index=True, right_index=True)
+employment_df = employment_df.merge(other_separations_df, how='outer', left_index=True, right_index=True)
+
+with st.container():
+    st.header('Job Openings and Labor Turnover Survey (JOLTS)')
+    st.text('JOLTS provides data on job openings, hires, and separations at the national and state level. The job openings rate can help measure the tightness of job markets.')
+    st.markdown('* Job Openings: All positions that are open (not filled) on the last business day of the month.')
+    st.markdown('* Hires: All additions to the payroll during the month.')
+    st.markdown('* Quits: Employees who left voluntarily. Exception: retirements or transfers to other locations are reported with Other Separations.')
+    st.markdown('* Layoffs & Discharges: Involuntary separations initiated by the employer.')
+    st.markdown('* Other Separations: Retirements, transfers to other locations; deaths; or separations due to employee disability.')
+
+    col1, col2 = st.columns([3, 1])
+
+    col1.line_chart(employment_df)
+    if (col2.checkbox('Show data', key='show_jolts_data')):
+        col2.dataframe(employment_df)
     
